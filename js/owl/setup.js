8f317d82814b8b13a1a1608ecf3c2fe73bce2fbd
Carlos <carlos_rj18@hotmail.com>
2021-12-15 02:58:59 -0300

    Finalizando o clone netflix

--- js/owl/setup.js
+++ js/owl/setup.js
@@ -1,12 +1,12 @@
 $('.owl-carousel').owlCarousel({
     loop:true,
     margin:10,
-    nav:true,
-    responsive:{
+    nav:true, /* mostra os próximos e anteriores  botão */
+    responsive:{ 
         0:{
             items:1
         },
-        500:{
+        600:{
             items:3
         },
         1000:{
